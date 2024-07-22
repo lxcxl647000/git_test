5f1beb6afd58143d27680cba589424a33c55d86d
lxcxl <lxcxl647000@gmail.com>
2024-07-22 23:42:50 +0800

    replace project

---
+++ vue3_admin_temp/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>hello</h1>
+  <button @click="count++">count:{{ count }}</button>
+</template>
+
+<script setup lang="ts">
+  import { ref } from 'vue';
+
+  let count = ref(0);
+</script>
+
+<style scoped></style>
