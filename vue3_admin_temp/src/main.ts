b991cdcf2369497d8f93acf06547e56fb9bde3a8
lxcxl <lxcxl647000@gmail.com>
2024-07-23 12:05:33 +0800

    modify old project

--- vue3_admin_temp/src/main.ts
+++ vue3_admin_temp/src/main.ts
@@ -1,4 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+//引入element-plus插件与样式
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import { zhCn } from 'element-plus/es/locales.mjs'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+//安装element-plus插件
+app.use(ElementPlus, {
+    locale: zhCn
+});
+app.mount('#app');
