7c12530df06bf90715262aafadbebabb83f4b7fd
lxcxl <lxcxl647000@gmail.com>
2024-07-24 12:02:05 +0800

    add vite-plugin-mock , add fake api and fake data

--- vue3_admin/src/App.vue
+++ vue3_admin/src/App.vue
@@ -1,13 +1,30 @@
 <template>
-  <!-- <SvgIcon name="home" color="red" width="50px" height="50px"></SvgIcon>
-  <SvgIcon name="loading" color="yellow" width="100px" height="100px"></SvgIcon> -->
   <div>
     <h1>hello</h1>
   </div>
+  <el-button type="primary" size="default" @click="testLogin">Login</el-button>
+  <h2 v-show="token">token is {{ token }}</h2>
 </template>
 
 <script setup lang="ts">
+  import axios from 'axios';
+  import { ref } from 'vue';
 
+  let token = ref('');
+
+  async function testLogin() {
+    let { data: { code, data } } = await axios.post('/api/user/login', {
+      username: 'admin',
+      password: '111111',
+    });
+    if (code === 200) {
+      alert('success login');
+      token.value = data.token;
+    }
+    else {
+      alert('fail login')
+    }
+  }
 </script>
 
 <style scoped lang="scss">
--- vue3_admin/vite.config.ts
+++ vue3_admin/vite.config.ts
@@ -4,29 +4,35 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 import path from 'path'
+import { viteMockServe } from 'vite-plugin-mock'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    createSvgIconsPlugin({
-      // Specify the icon folder to be cached
-      iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
-      // Specify symbolId format
-      symbolId: 'icon-[dir]-[name]',
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        javascriptEnabled: true,
-        additionalData: `@import "@/styles/_variables.scss";`
+    plugins: [
+      vue(),
+      createSvgIconsPlugin({
+        // Specify the icon folder to be cached
+        iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
+        // Specify symbolId format
+        symbolId: 'icon-[dir]-[name]',
+      }),
+      viteMockServe({
+        // default
+        mockPath: 'mock',
+        enable: true,
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
       }
-    }
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          javascriptEnabled: true,
+          additionalData: `@import "@/styles/_variables.scss";`
+        }
+      }
   }
 })
