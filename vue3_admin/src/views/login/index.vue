7daba610fc1f394813479604fc90ed8869d2dac6
lxcxl <lxcxl647000@gmail.com>
2024-07-26 15:46:27 +0800

    add login form validate

--- vue3_admin/src/views/login/index.vue
+++ vue3_admin/src/views/login/index.vue
@@ -3,13 +3,13 @@
         <el-row>
             <el-col :span="12" :offset="0" :xs="0"></el-col>
             <el-col :span="12" :offset="0" :xs="24">
-                <el-form class="login_form">
+                <el-form class="login_form" :model="loginData" :rules="rules" ref="loginForm">
                     <h1>Hello</h1>
                     <h2>欢迎来到后台管理</h2>
-                    <el-form-item>
+                    <el-form-item prop="username">
                         <el-input :prefix-icon="User" v-model="loginData.username"></el-input>
                     </el-form-item>
-                    <el-form-item>
+                    <el-form-item prop="password">
                         <el-input :prefix-icon="Lock" type="password" v-model="loginData.password"
                             show-password></el-input>
                     </el-form-item>
@@ -27,6 +27,7 @@
 <script setup lang="ts">
     import type { ILogin } from '@/api/user/type';
     import useUserStore from '@/store/modules/user';
+    import { getCurrentTimeDes } from '@/utils/time';
     import { User, Lock } from '@element-plus/icons-vue';
     import { ElNotification } from 'element-plus';
     import { reactive, ref } from 'vue';
@@ -42,22 +43,46 @@
 
     let loading = ref(false);
 
+    // 规则对象属性//
+    // required 表示该字段必须校验//
+    // min 文本最小长度//
+    // max 文本最大长度//
+    // message 错误提示信息//
+    // trigger 处罚校验时机 blur失去焦点  change文本发生改变//
+    let rules = reactive({
+        username: [
+            { required: true, message: '名字长度6到10位', min: 6, max: 10, trigger: 'change' }
+        ],
+        password: [
+            { required: true, message: '密码长度至少6位', min: 6, trigger: 'change' }
+        ]
+    });
+
+    let loginForm = ref();
+
     async function login() {
-        loading.value = true;
         try {
-            await userStore.userLogin(loginData);
-            ElNotification({
-                type: 'success',
-                message: '登录成功'
-            });
-            router.push('/');
-            loading.value = false;
-        } catch (error: any) {
-            ElNotification({
-                type: 'error',
-                message: error.message
-            });
-            loading.value = false;
+            await loginForm.value.validate();
+
+            loading.value = true;
+            try {
+                await userStore.userLogin(loginData);
+                ElNotification({
+                    type: 'success',
+                    message: '欢迎回来',
+                    title: `Hi,${getCurrentTimeDes()}好`,
+                });
+                router.push('/');
+                loading.value = false;
+            } catch (error: any) {
+                ElNotification({
+                    type: 'error',
+                    message: error.message
+                });
+                loading.value = false;
+            }
+        } catch (error) {
+
         }
     }
 </script>
