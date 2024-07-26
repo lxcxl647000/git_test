83af07fae3e220ec51d291a7f0559ac869091958
lxcxl <lxcxl647000@gmail.com>
2024-07-26 16:13:46 +0800

    modify login form validate(custom validate)

--- vue3_admin/src/views/login/index.vue
+++ vue3_admin/src/views/login/index.vue
@@ -51,10 +51,12 @@
     // trigger 处罚校验时机 blur失去焦点  change文本发生改变//
     let rules = reactive({
         username: [
-            { required: true, message: '名字长度6到10位', min: 6, max: 10, trigger: 'change' }
+            // { required: true, message: '名字长度6到10位', min: 6, max: 10, trigger: 'change' }
+            { validator: validateUserName, trigger: 'change' }
         ],
         password: [
-            { required: true, message: '密码长度至少6位', min: 6, trigger: 'change' }
+            // { required: true, message: '密码长度至少6位', min: 6, trigger: 'change' }
+            { validator: validatePassword, trigger: 'change' }
         ]
     });
 
@@ -83,6 +85,24 @@
             }
         } catch (error) {
 
+        }
+    }
+
+    function validateUserName(rule: any, value: any, callback: any) {
+        if (value.length < 5 || value.length > 10) {
+            callback(new Error('名字长度5到10位'));
+        }
+        else {
+            callback();
+        }
+    }
+
+    function validatePassword(rule: any, value: any, callback: any) {
+        if (value.length < 6) {
+            callback(new Error('密码长度至少6位'));
+        }
+        else {
+            callback();
         }
     }
 </script>
