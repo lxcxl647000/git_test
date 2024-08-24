73bc651357947c5ef7e62d1ce7c518a583362be4
lxcxl <lxcxl647000@gmail.com>
2024-08-24 16:18:55 +0800

    data screen top interface

--- vue3_admin/src/views/screen/Screen.vue
+++ vue3_admin/src/views/screen/Screen.vue
@@ -1,9 +1,55 @@
 <template>
-    <h1>数据大屏</h1>
+    <div class="container">
+        <div class="screen" ref="screen">
+            <div class="top">
+                <ScreenTop></ScreenTop>
+            </div>
+            <div class="bottom"></div>
+        </div>
+
+    </div>
 </template>
 
 <script setup lang="ts">
+    import { onMounted, ref } from 'vue';
+    import ScreenTop from './components/ScreenTop.vue';
 
+    let screen = ref();
+
+    onMounted(() => {
+        screen.value.style.transform = `scale(${getScale()}) translate(-50%,-50%)`;
+    });
+
+    function getScale(w = 1920, h = 1080) {
+        let ww = window.innerWidth / w;
+        let wh = window.innerHeight / h;
+        return ww < wh ? ww : wh;
+    }
+
+    window.onresize = () => {
+        screen.value.style.transform = `scale(${getScale()}) translate(-50%,-50%)`;
+    }
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+    .container {
+        width: 100vw;
+        height: 100vh;
+        background: url(./images/bg.png) no-repeat;
+        background-size: cover;
+
+        .screen {
+            position: fixed;
+            width: 1920px;
+            height: 1080px;
+            transform-origin: top left;
+            left: 50%;
+            top: 50%;
+
+            .top {
+                width: 100%;
+                height: 40px;
+            }
+        }
+    }
+</style>
---
+++ vue3_admin/src/views/screen/components/ScreenTop.vue
@@ -0,0 +1,102 @@
+<template>
+    <div class="top">
+        <div class="left">
+            <span class="lbt" @click="goHome" style="cursor: pointer;">首页</span>
+        </div>
+        <div class="center">智慧旅游可视化大数据展示平台</div>
+        <div class="right">
+            <span class="rbt" style="cursor: pointer;">统计报告</span>
+            <span class="time">当前时间：{{ timeStr }}</span>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+    import dayjs from 'dayjs';
+    import { onBeforeUnmount, onMounted, ref } from 'vue';
+    import { useRouter } from 'vue-router';
+
+    let router = useRouter();
+
+    let timer = ref(0);
+    let timeStr = ref(dayjs().format('YYYY年MM月DD日hh:mm:ss'));
+
+    onMounted(() => {
+        timer.value = setInterval(() => {
+            timeStr.value = dayjs().format('YYYY年MM月DD日HH:mm:ss');
+        }, 1000);
+    });
+
+    onBeforeUnmount(() => {
+        clearInterval(timer.value);
+    });
+
+    function goHome() {
+        router.push('/home');
+    }
+</script>
+
+<style scoped lang="scss">
+    .top {
+        display: flex;
+
+        .left {
+            flex: 1.5;
+            background: url(../images/dataScreen-header-left-bg.png) no-repeat;
+            background-size: cover;
+
+            .lbt {
+                width: 135px;
+                height: 42px;
+                background: url(../images/dataScreen-header-btn-bg-l.png) no-repeat;
+                background-size: cover;
+                float: right;
+                text-align: center;
+                line-height: 40px;
+                font-size: 20px;
+                color: #29fcff;
+            }
+        }
+
+        .center {
+            flex: 2;
+            width: 100%;
+            height: 74px;
+            background: url(../images/dataScreen-header-center-bg.png) no-repeat;
+            background-size: cover;
+            text-align: center;
+            line-height: 70px;
+            font-size: 30px;
+            color: #29fcff;
+        }
+
+        .right {
+            flex: 1.5;
+            background: url(../images/dataScreen-header-right-bg.png) no-repeat;
+            background-size: cover;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+
+            .rbt {
+                width: 135px;
+                height: 42px;
+                background: url(../images/dataScreen-header-btn-bg-r.png) no-repeat;
+                background-size: cover;
+                float: left;
+                text-align: center;
+                line-height: 40px;
+                font-size: 20px;
+                color: #29fcff;
+            }
+
+            .time {
+                margin-right: 10px;
+                text-align: center;
+                line-height: 40px;
+                font-size: 20px;
+                color: #29fcff;
+            }
+        }
+    }
+</style>
