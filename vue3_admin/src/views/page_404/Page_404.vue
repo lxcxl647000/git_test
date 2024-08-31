d4de7f9bf4340bf2425783cf756da2de34d8e273
lxcxl <lxcxl647000@gmail.com>
2024-08-31 11:14:55 +0800

    page 404

--- vue3_admin/src/views/page_404/Page_404.vue
+++ vue3_admin/src/views/page_404/Page_404.vue
@@ -1,9 +1,37 @@
 <template>
-    <h1>一级路由---404</h1>
+    <div class="bg">
+        <img src="../../assets/images/error_images/401.png" alt="">
+        <el-button type="primary" size="large" @click="goHome" class="btn">首页</el-button>
+    </div>
 </template>
 
 <script setup lang="ts" name="404">
+    import { useRouter } from 'vue-router';
 
+    let router = useRouter();
+
+    function goHome() {
+        router.push({ path: '/home' });
+    }
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+    .bg {
+        width: 100vw;
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        background: skyblue;
+
+        img {
+            width: 1014px;
+            height: 556px;
+        }
+
+        .btn {
+            position: relative;
+            left: -30%;
+            top: 70%;
+        }
+    }
+</style>
